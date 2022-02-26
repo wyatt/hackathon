c105606660de2de3057900d5f71bb17591fe90c7
wyatt <profwyattb@gmail.com>
2022-02-26 23:29:57 +0000

    nextjs config

--- apps/frontend/react-scroll-text.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'react-scroll-text' {
-	export interface ScrollTextProps {
-		speed?: number;
-		children: string;
-	}
-
-	declare const ScrollText: JSX.Element<ScrollTextProps>;
-	export = ScrollText;
-}
