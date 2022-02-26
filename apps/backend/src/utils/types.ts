369d8a2870e391c48bb22524e889ff15bddd30fb
wyatt <profwyattb@gmail.com>
2022-02-26 23:47:53 +0000

    Removed other crap

--- apps/backend/src/utils/types.ts
+++ apps/backend/src/utils/types.ts
@@ -1,11 +1,3 @@
-import {Lesson, Task, TaskInfo} from '@prisma/client';
-
 export type StrictOmit<T, E extends keyof T> = Omit<T, E> & {
 	[key in E]?: never;
-};
-
-export type ExtendedUser<T> = T & {
-	lessons: (Lesson & {
-		tasks: (Task & {info: TaskInfo})[];
-	})[];
 };
